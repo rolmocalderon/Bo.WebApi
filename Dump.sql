6dd03eba9eabe4605389cebe7ac869d4c3ce4802
rolmocalderon <r.olmocalderon@gmail.com>
2022-01-18 18:39:17 +0100

    new database

--- Dump.sql
+++ Dump.sql
@@ -25,14 +25,9 @@ DROP TABLE IF EXISTS `pickups`;
 CREATE TABLE `pickups` (
   `id` int NOT NULL AUTO_INCREMENT,
   `date` varchar(45) NOT NULL,
-  `productId` int NOT NULL,
-  `amount` int DEFAULT NULL,
-  `observations` varchar(200) DEFAULT NULL,
-  `placeId` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `placeId_idx` (`placeId`),
-  CONSTRAINT `placeId` FOREIGN KEY (`placeId`) REFERENCES `places` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,32 +36,40 @@ CREATE TABLE `pickups` (
 
 LOCK TABLES `pickups` WRITE;
 /*!40000 ALTER TABLE `pickups` DISABLE KEYS */;
-INSERT INTO `pickups` VALUES (1,'17/1/2022',1,15,NULL,1),(2,'16/1/2022',2,12,NULL,1),(3,'15/1/2022',3,15,NULL,2),(4,'15/1/2022',2,3,NULL,2);
+INSERT INTO `pickups` VALUES (1,'17/1/2022','Mercadona Nou Barris'),(2,'16/1/2022','Spar Poble Sec'),(3,'15/1/2022','Mercadona Nou Barris'),(4,'15/1/2022','Spar Poble Sec'),(5,'14/1/2022','asdf'),(6,'13/1/2022','trytryrt'),(7,'12/1/2022','trytryrt'),(8,'21/1/2022','fffrrrtt');
 /*!40000 ALTER TABLE `pickups` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `places`
+-- Table structure for table `productpicked`
 --
 
-DROP TABLE IF EXISTS `places`;
+DROP TABLE IF EXISTS `productpicked`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `places` (
+CREATE TABLE `productpicked` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `amount` int NOT NULL,
+  `Observations` varchar(200) DEFAULT NULL,
+  `weight` varchar(45) DEFAULT NULL,
+  `pickupId` int DEFAULT NULL,
+  `productId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productId_idx` (`productId`),
+  KEY `pickupId_idx` (`pickupId`),
+  CONSTRAINT `pickupId` FOREIGN KEY (`pickupId`) REFERENCES `pickups` (`id`),
+  CONSTRAINT `productId` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `places`
+-- Dumping data for table `productpicked`
 --
 
-LOCK TABLES `places` WRITE;
-/*!40000 ALTER TABLE `places` DISABLE KEYS */;
-INSERT INTO `places` VALUES (1,'Mercadona nou barris'),(2,'Bon Preu Sant Andreu');
-/*!40000 ALTER TABLE `places` ENABLE KEYS */;
+LOCK TABLES `productpicked` WRITE;
+/*!40000 ALTER TABLE `productpicked` DISABLE KEYS */;
+INSERT INTO `productpicked` VALUES (1,5,NULL,'500',1,1),(2,3,NULL,'200',2,2),(3,15,NULL,'100',1,3);
+/*!40000 ALTER TABLE `productpicked` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -129,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-17 22:24:25
+-- Dump completed on 2022-01-18 18:38:54

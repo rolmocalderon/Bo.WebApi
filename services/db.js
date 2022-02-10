c5765e9c831d71605cc25f710a6f24a339352ffa
Ruben del Olmo <R.OlmoCalderon@gmail.com>
2022-02-10 19:42:23 +0100

    debug

--- services/db.js
+++ services/db.js
@@ -6,15 +6,17 @@ const postgre = require('./postgresql');
 async function query(sql, params) {
   //const connection = await mysql.createConnection(config.db);
   //const [results, ] = await connection.execute(sql, params);
-  console.log("doing query", sql)
-  const responses = await postgre.query(sql)
-  console.log("response", responses)
+  console.log("doing query", sql);
+  const responses = await postgre.query(sql);
+  console.log("query response", responses);
   return responses.rows;
 }
 
 async function getAll(table, params){
+  console.log("doing query", table, params);
   const connection = await mysql.createConnection(config.db);
   const [results] = await connection.execute("SELECT * from " + table, params);
+  console.log("getAll", results);
 
   return results;
 }

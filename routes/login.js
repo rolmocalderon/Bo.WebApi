7e7b9e25ef54083b20bb6731dd4fd6975ddb68d0
Ruben del Olmo <R.OlmoCalderon@gmail.com>
2022-02-10 19:28:15 +0100

    asdf

--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,7 @@ router.get('/', async function(req, res, next) {
   try {
         console.log("requesting loing", req.query)
         if(typeof req != 'object'){
+            console.log("is failing because of this")
             req.query = JSON.parse(req.query)
         }
         res.json(await login.getSingle(req.query));

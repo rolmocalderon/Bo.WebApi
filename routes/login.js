eb3c209f8a71ee57c9297f6a522ea9ccf7633045
Ruben del Olmo <R.OlmoCalderon@gmail.com>
2022-10-14 23:04:11 +0200

    login post

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const login = require('../services/login');
 
-router.get('/', async function(req, res, next) {
+router.post('/', async function(req, res, next) {
   try {
-        res.json(await login.getSingle(req.query));
+        res.json(await login.getSingle(req.body));
   } catch (err) {
         console.error(`Error while loging `, err.message);
         next(err);

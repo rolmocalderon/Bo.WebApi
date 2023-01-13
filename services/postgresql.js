999df32fb7a221005380fb774c93827532ed5f19
Ruben del Olmo <R.OlmoCalderon@gmail.com>
2023-01-13 19:27:03 +0100

    add env

--- services/postgresql.js
+++ services/postgresql.js
@@ -1,17 +1,23 @@
 //POOL POSTGRESQL
 const Pool = require("pg").Pool;
-
+require('dotenv').config()
+const connectionString = `postgres://gcktxbujwxrejb:b716a0a366e92546ce3b67feaf1972913610d7144c905f7ce04fa5cb2869a8c8@ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d6tirr3ic3t3r9`;
+console.log("LOGING", process.env.PGHOST)
 const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
     ssl: {
         rejectUnauthorized: false
     }
   })
 module.exports = pool;
+
+//HEROKU
+//psql -h ec2-52-213-167-210.eu-west-1.compute.amazonaws.com -d d6tirr3ic3t3r9 -U gcktxbujwxrejb
+//psql -h mel.db.elephantsql.com/ppdrywgj -d ppdrywgj -U ppdrywgj
 
 //ELEPHANT
 //postgres://ppdrywgj:DNiNrZu9Urz6amyqZgp7FRthhMb__lrs@mel.db.elephantsql.com/ppdrywgj
